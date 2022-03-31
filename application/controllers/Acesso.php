84eb6af77c960129f6579405938bec1e63b88623
allanneros <allan@neros.com.br>
2022-03-31 08:06:20 -0300

    v1.10 Alteração no controle de sessão

--- application/controllers/Acesso.php
+++ application/controllers/Acesso.php
@@ -1,4 +1,8 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed');
+<?php 
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+require(APPPATH . '/controllers/mail/MailNotificacoes.php');
+
 
 //Controller Acesso
 //Neros Labs
@@ -11,29 +15,47 @@ class Acesso extends CI_Controller {
 
     public function index() {
         //Exibe a tela de autenticação  
+        $data['pageTitle'] = "Acesso ao sistema";
+        $data['retorno'] = "";
+
         $form_login = $this->input->post('form_login');
         $form_senha = $this->input->post('form_senha');
-        
+ 
         if ($form_login == NULL) {
-            $this->load->view('acesso/login');
+            $data['retorno'] = "";
         } else {
             $acesso = $this->autenticar($form_login,$form_senha);
+
             if ($acesso != NULL) {
+                //Verifica se o usuário está ativo
                 $this->iniciarSessao($acesso);
-                redirect(base_url() . index_page() . '/inicio');
+                $sessao = lerSessaoAtual();
+                
+                if (!$sessao['usuario_ativo']) {
+                    $data['retorno'] = "Seu usuário está inativo na plataforma.";
+
+                } else {
+                    redirect(base_url() . index_page() . '/inicio');
+
+                }
+                
             } else {
                 $data['retorno'] = 'Usu&aacute;rio ou senha inv&aacute;lidos.';
-                $this->load->view('acesso/login',$data);
+
             }
         }
+
+        $this->load->view('header',$data);
+        $this->load->view('acesso/login',$data);
+        $this->load->view('footer',$data);
     }
 
-    //Recuperar
-    //Serve para o usuário recuperar o acesso
-    //O usuário deverá informar um e-mail válido e se encontrado o sistema irá gerar uma chave 
-    //  temporária para que ele possa definir uma nova senha
-    function recuperar($chave=NULL){
+    //Ativar
+    //Serve para o usuário ativar a sua conta no site
+    function ativar($chave=NULL){
         $this->load->model('usuario_model');
+
+        $data['pageTitle'] = "Ativação de novo usuário";
 
         //Se nao tiver chave, significa que é um novo pedido de senha
         if ($chave==NULL) {
@@ -41,7 +63,7 @@ class Acesso extends CI_Controller {
 
             //Se for a primeira visita exibir o form de solicitação
             if ($form_login == NULL) {
-                $this->load->view('acesso/recuperar');
+                $data = NULL;
             } else {
                 //Caso contrário, localizar o e-mail no sistema... 
                 $usuario = $this->usuario_model->localizarUsuario($form_login);
@@ -51,13 +73,17 @@ class Acesso extends CI_Controller {
                     $chave = $this->usuario_model->gerarChaveAcesso($usuario['id']);
                     //...e mandar por e-mail para o cara
                     if ($chave != NULL) {
-                        enviar_email($usuario['login'],'Sua chave de acesso','Chave: ' . $chave);
+                        //TODO: aplicar o modelo de notificação para ativação
+                        //enviar_email($usuario['login'],'Sua chave de acesso','Chave: ' . $chave);
                         $data['retorno'] = 'Foi gerada uma chave de acesso e enviada em seu e-mail.';
-                        $this->load->view('acesso/recuperar',$data);
                     }
                 }
                 
             }
+
+            $this->load->view('header',$data);
+            $this->load->view('acesso/ativar',$data);
+            $this->load->view('footer',$data);
 
         //Se foi gerada uma chave, verificar se confere com o que está no registro e reativa o cara
         } else {
@@ -70,11 +96,108 @@ class Acesso extends CI_Controller {
         }
     }
 
+    //Recuperar
+    //Serve para o usuário recuperar o acesso
+    //O usuário deverá informar um e-mail válido e se encontrado o sistema irá gerar uma chave 
+    //  temporária para que ele possa definir uma nova senha
+    function recuperar(){
+        $this->load->model('usuario_model');
+
+        $form_login = $this->input->post('form_login');
+
+        $data['pageTitle'] = "Recuperar acesso";
+
+        //Se for a primeira visita exibir o form de solicitação
+        if ($form_login == NULL) {
+            $data = NULL;
+        } else {
+            //Caso contrário, localizar o e-mail no sistema... 
+            $usuario = $this->usuario_model->localizarUsuario($form_login);
+            
+            //...e se encontrado gerar uma chave de acesso...
+            if ($usuario != NULL) {
+                $chave = $this->usuario_model->gerarChaveAcesso($usuario['id']);
+                //...e mandar por e-mail para o cara
+                if ($chave != NULL) {
+                    $info = Array(
+                        'mailRecipients'    => $usuario['login'],
+                        'recoverKey'        => $chave,
+                    );
+                    $result = MailNotificacoes::enviarNotificacaoRecuperacao($info);
+
+                    $data['retorno'] = 'Foi gerada uma chave de acesso e enviada em seu e-mail.';
+                }
+            } else {
+                $data['retorno'] = 'Não foi localizado usuário com este e-mail. Verifique e tente novamente.';
+            }
+                
+        }
+
+        $this->load->view('header',$data);
+        $this->load->view('acesso/recuperar',$data);
+        $this->load->view('footer',$data);
+
+    }
+
+    //Redefinir
+    //Serve para alterar a senha de acesso
+    function redefinir($chave=NULL){
+        $this->load->model('usuario_model');
+
+        $data['pageTitle']  = "Redefinir senha de acesso";
+        $data['form_chave'] = "";
+
+        $chave      = $this->input->get('chave', TRUE);
+        $form_chave = $this->input->post('form_chave');
+        $form_senha = $this->input->post('form_senha');
+
+        //Se a chave foi fornecida na URL, preenche o campo do form
+        if ($chave) {
+            $data['form_chave'] = $chave;
+
+        //Caso contrário, verifica se a chave está no form (post)
+        } elseif (!$form_chave) {
+            $data['retorno'] = "Chave inválida";
+            //redirect(base_url() . index_page() . '/acesso');
+
+        //Senão, segue o processo para atualização da senha
+        } elseif (!$form_senha) {
+            $data['retorno'] = "Senha não foi informada";
+
+        } else {
+            $registro = $this->usuario_model->localizarUsuarioChave($form_chave);
+
+            if ($registro) {
+                //Se a chave confere, ativa o usuário e retorna para a tela de login
+                $return = $this->usuario_model->atualizarSenhaUsuario($registro['id'],$form_senha);
+                
+                if ($return != NULL) {
+                    $data['retorno'] = "Senha redefinida com sucesso.";
+                    redirect(base_url() . index_page() . '/inicio');
+
+                } else {
+                    $data['retorno'] = "Não foi possível alterar a senha.";
+
+                }
+
+            } else {
+                $data['retorno'] = "Chave não encontrada.";
+            }
+
+        }
+
+        $this->load->view('header',$data);
+        $this->load->view('acesso/redefinir',$data);
+        $this->load->view('footer',$data);
+    }
+
 
     //Registrar
     //Serve para o aluno se cadastrar no site
     public function registrar($chave=NULL) {
         $this->load->model('usuario_model');
+
+        $data['pageTitle'] = "Cadastre-se";
 
         if ($chave == NULL) {
             $form_login             = $this->input->post('form_login');
@@ -83,12 +206,11 @@ class Acesso extends CI_Controller {
             $form_senha_confirmacao = $this->input->post('form_senha_confirmacao');
             
             if ($form_login == NULL || $form_nome == NULL || $form_senha == NULL) {
-                $this->load->view('acesso/registro');
+                $data = NULL;
             } else {
                 //Senhas conferem?
                 if ($form_senha != $form_senha_confirmacao) {
                     $data['retorno'] = 'Senhas n&atilde;o conferem';
-                    $this->load->view('acesso/registro',$data);
                 } else {
                     //Faz um hash na senha para encriptar
                     $form_senha = hash('sha512',$form_senha);
@@ -100,16 +222,16 @@ class Acesso extends CI_Controller {
                     $registro = $this->usuario_model->registrarAluno($form_login,$form_senha,$form_nome,$chave);
                     if ($registro) {
                         //Se registrou o aluno, envia a chave de ativação por e-mail
-                        enviar_email_registro($form_login,$form_nome,$chave);
+                        $info['activationKey']      = $chave;
+                        $info['$mailRecipients']    = $form_login;
 
-                        $form_login = NULL;
-                        $form_senha = NULL;
-                        $form_nome  = NULL;
-                        
+                        $e = MailNotificacoes::enviarNotificacaoRegistro($info);
+
                         redirect(base_url() . index_page() . '/inicio');
                     }
                 }
             }
+
         } else {
             //Se tem chave de ativação, localiza e ativa o usuário
             $registro = $this->usuario_model->localizarUsuarioChave($chave);
@@ -120,24 +242,21 @@ class Acesso extends CI_Controller {
             } else {
                 //Caso contrário, exibe a mensagem e volta para a tela de login
                 $data['retorno'] = 'Chave de ativação inválida.';
-                $this->load->view('acesso/login',$data);
             }
         }
 
+        $this->load->view('header',$data);
+        $this->load->view('acesso/registrar',$data);
+        $this->load->view('footer',$data);
 
     }
     
     //Autentica o usuário no sistema
-    //TODO: criptografar a senha
     public function autenticar($usuario,$senha) {
         $this->load->model('usuario_model');
 
         //Aqui é necessário fazer o hash da senha para mandar ao banco de dados
-        //echo('Senha digitada: ' . $senha . '<br>');
-        //var_dump($senha);
         $senha = hash('sha512',$senha);
-        //echo('Senha com hash SHA-512: ' . $senha . '<br>');
-        //var_dump($senha);
 
         //Faz a busca do usuário no sistema e se encontrar preenche os dados de sessão
         $data = $this->usuario_model->autenticarUsuario($usuario,$senha);
@@ -157,11 +276,6 @@ class Acesso extends CI_Controller {
                 'usuario_professor' => $data['professor']
             );
             
-            //var_dump($data['id']);
-            //var_dump($data['nome']);
-            //var_dump($data['ativo']);
-            //var_dump($data['login']);
-    
             $this->session->set_userdata($sessao);
             
         } else {
--- application/helpers/sessao_helper.php
+++ application/helpers/sessao_helper.php
@@ -4,22 +4,69 @@
 //Allan Neros
 //Contem funcoes relacionadas a sessao
 
+if ( !function_exists('verificarSessaoAtiva')) {
+    function verificarSessaoAtiva() {
+        //Verifica se o usuário tem acesso administrativo
+        $CI =& get_instance();
+        $r = usuarioLogado();
+
+        if (!usuarioLogado()) {
+            redirect(base_url() . index_page() . '/acesso');
+
+        }
+    }   
+}
+
+if ( !function_exists('usuarioAtivo')) {
+    function usuarioAtivo() {
+        //Verifica se o usuário tem acesso administrativo
+        $CI =& get_instance();
+
+        $data = lerSessaoAtual();
+
+        return $data['usuario_ativo'];
+    }   
+}
+
+if ( !function_exists('usuarioProfessor')) {
+    function usuarioProfessor() {
+        $CI =& get_instance();
+        $data = lerSessaoAtual();
+
+        return $data['usuario_professor'];
+    }   
+}
+
+if ( !function_exists('usuarioAdmin')) {
+    function usuarioAdmin() {
+        $CI =& get_instance();
+        $data = lerSessaoAtual();
+
+        return $data['usuario_admin'];
+    }   
+}
+
+if ( !function_exists('usuarioLogado')) {
+    function usuarioLogado() {
+        $CI =& get_instance();
+        $data = lerSessaoAtual();
+
+        return ($data['usuario_id'] == NULL ? FALSE : TRUE);
+
+    }   
+}
+
 if ( !function_exists('lerSessaoAtual')) {
     function lerSessaoAtual() {
         $CI =& get_instance();
 
         //Carrega os dados da sessão atual e devolve um array que pode ser trabalhado no controller e na view
-        $usuario_id         = $CI->session->userdata('usuario_id');
-        $usuario_nome       = $CI->session->userdata('usuario_nome');
-        $usuario_login      = $CI->session->userdata('usuario_login');
-        $usuario_admin      = $CI->session->userdata('usuario_admin');
-        $usuario_professor  = $CI->session->userdata('usuario_professor');
-
-        $data['usuario_id']         = $usuario_id;
-        $data['usuario_nome']       = $usuario_nome;
-        $data['usuario_login']      = $usuario_login;
-        $data['usuario_admin']      = $usuario_admin;
-        $data['usuario_professor']  = $usuario_professor;
+        $data['usuario_id']         = $CI->session->userdata('usuario_id');
+        $data['usuario_nome']       = $CI->session->userdata('usuario_nome');
+        $data['usuario_ativo']      = $CI->session->userdata('usuario_ativo');
+        $data['usuario_login']      = $CI->session->userdata('usuario_login');
+        $data['usuario_admin']      = $CI->session->userdata('usuario_admin');
+        $data['usuario_professor']  = $CI->session->userdata('usuario_professor');
 
         return $data;
     }   
