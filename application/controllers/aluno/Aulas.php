10ce8c0b1a04a9904ec1d65ccc0df9c90d329563
allanneros <allan@neros.com.br>
2022-05-07 04:04:33 -0300

    v1.26 Inclusão do material de apoio na visualização da aula (Aluno)

--- application/controllers/aluno/Aulas.php
+++ application/controllers/aluno/Aulas.php
@@ -19,22 +19,34 @@ class Aulas extends CI_Controller {
             redirect(base_url() . index_page() . '/inicio');
         }
 
-        $data = lerSessaoAtual();
-        $aula = $this->aula_model->lerAula($id_aula);
+        $data                   = lerSessaoAtual();
+        $data['aula']           = $this->aula_model->lerAula($id_aula);
+        $data['aula_arquivos']  = $this->_listarMaterialAula($id_aula);
 
-        if ($aula == NULL) {
+        if ($data['aula'] == NULL) {
             //Se não encontrou o curso, deve retornar a página inicial
             redirect(base_url() . index_page() . '/aluno/cursos');
         } else {
             //Carrega a página de visualização do curso e das aulas
-            $data['pageTitle'] = "Visualizar aula";
-            $data['aula'] = $aula;
+            $data['pageTitle']      = "Visualizar aula";
 
             $this->load->view('_restrito/header',$data);
             $this->load->view('aluno/navbar');
             $this->load->view('aluno/aulas/visualizar',$data);
             $this->load->view('_restrito/footer',$data);
         }
+    }
+
+    //Lista os materiais de apoio
+    private function _listarMaterialAula($id_aula) {
+        $material = $this->aula_model->listarMateriais($id_aula);
+        if (!$material) {
+            $material   = array(
+                            'nome'      => '',
+                            'arquivo'   => ''
+                        );
+        }
+        return $material;
     }
 
 }
--- application/views/aluno/aulas/visualizar.php
+++ application/views/aluno/aulas/visualizar.php
@@ -1,18 +1,21 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
 
-	<!-- Content Wrapper. Contains page content -->
+<script src="<?php echo(base_url() . HAND_TALK_JS); ?>"></script>
+<script src="<?php echo(base_url() . 'includes/handtalk/ht-video-player.min.js'); ?>"></script>
+
+    <!-- Content Wrapper. Contains page content -->
 	<div class="content-wrapper">
         <!-- Content Header (Page header) -->
         <section class="content-header">
-            <h1>Aulas 
-                <small>Assistir.</small>
-                <button id="button_voltar" class="btn btn-default">Voltar para o curso</button>
+            <h1>Assista a aula 
+                <small></small>
+                <button id="button_voltar" class="btn btn-default"><i class="fa fa-arrow-left"></i> Voltar para o curso</button>
             </h1>
             <ol class="breadcrumb">
-                <li><a href="#"><i class="fa fa-dashboard"></i> Início</a></li>
+                <li><a href="#"><i class="fa fa-house"></i> Início</a></li>
                 <li class="active">Cursos</li>
                 <li class="active">Aulas</li>
-                <li class="active">Visualizar</li>
+                <li class="active"><?php echo($aula['tema']); ?></li>
             </ol>
         </section>
 
@@ -20,37 +23,77 @@
         <section class="content">
 			<!-- Your Page Content Here -->
             <div class="row">
-                <div class="col-sm-6">
+                <div class="col-sm-8">
                     <div class="box box-danger">
                         <div class="box-header with-border">
-                            <h3 class="box-title">Video</h3>
-                        </div><!-- /.box-header -->
+                            <h3 class="box-title"><?php echo($aula['tema']); ?></h3>
+                        </div>
+
                         <div class="box-body">
-                            <?php if ($aula['arquivo_video'] != NULL) { ?>
+                            <?php 
+                                if ($aula['arquivo_video'] != NULL) { 
+                                $video_url      = $aula['arquivo_video'];
+                                $video_url      = str_replace('http://','https://',$video_url);  
 
-                                <?php echo($aula['arquivo_video']); ?>
-                                <br>
-                                <?php echo(str_replace('mp4','vtt',$aula['arquivo_video'])); ?>
+                                $video_subtitle = $aula['arquivo_legenda']; 
+                                if ($video_subtitle)  {
+                                    $video_subtitle = str_replace('http://','https://',$video_subtitle);  
+                                } else {
+                                    $video_subtitle = str_replace('/video/upload','/raw/upload',$video_url);  
+                                }
 
-                                <video 
-                                    data-ht-src-type="webm"
-                                    data-ht-src="<?php echo($aula['arquivo_video']); ?>"
-                                    data-ht-subtitle-type="vtt"
-                                    data-ht-subtitle="<?php echo(str_replace('mp4','vtt',$aula['arquivo_video'])); ?>">
-                                </video>
+                                $video_subtitle = str_replace('.mp4','.srt',$video_subtitle);  
+                                $video_subtitle = str_replace('.webm','.srt',$video_subtitle);  
 
-                            <?php } else { echo('<p>Esta aula não tem vídeo.</p>');} ?>
+                                echo('<video ');
+                                echo('data-ht-src-type="webm" ');
+                                echo('data-ht-src="' . $video_url . '" ');
+                                echo('data-ht-subtitle-type="srt"');
+                                echo('data-ht-subtitle="' . $video_subtitle . '">');
+                                echo('</video>');
+
+                                } else { 
+                                    echo('<p>Esta aula não tem vídeo.</p>');
+                                } 
+                            ?>
                         </div><!-- /.box-body -->
+
+                        <div class="box-footer">
+                            <?php echo($aula['resumo']); ?>
+                        </div>
+
                     </div><!-- /.box -->
+
                 </div><!-- /.col -->
-                <div class="col-sm-6">
-                    <div class="box box-primary">
+                <div class="col-sm-4">
+                    <div class="box box-success">
                         <div class="box-header with-border">
-                            <h3 class="box-title">Informações desta aula</h3>
+                            <h3 class="box-title"><i class="fa fa-file"></i> Material de apoio</h3>
                         </div>
                         <div class="box-body">
-                            <b><?php echo($aula['tema']); ?></b>
-                            <p><?php echo($aula['resumo']); ?></p>
+                        <?php
+                            if ($aula_arquivos) {
+                                echo('<table class="table">');
+                                echo('<tr>');
+                                echo('<th>Arquivo</th>');
+                                echo('</tr>');
+
+                                foreach($aula_arquivos as $arquivo) {
+                                    if (isset($arquivo['nome'])) {
+                                        echo('<tr>');
+                                        echo('<td>');
+                                        echo('<a href="' . base_url() . $arquivo['arquivo'] . '">');
+                                        echo($arquivo['nome']);
+                                        echo('</a>');
+                                        echo('</td>');
+                                        echo('</tr>');
+                                    }
+                                }
+
+                                echo('</table>');
+                            }
+                        ?>
+
                         </div>
                         <!-- /.box-body -->
                         <div class="box-footer">
@@ -67,8 +110,11 @@
     }
 </script>
 
+<!--
 <script src="https://plugin.handtalk.me/web/latest/handtalk.min.js"></script>
 <script src="<?php echo(base_url() . HAND_TALK_JS); ?>"></script>
+<script src="<?php echo(base_url() . 'includes/handtalk/ht-video-player.min.js'); ?>"></script>
+-->
 <script>
     var ht = new HT({
         token: "<?php echo(HAND_TALK_API_TOKEN); ?>",
