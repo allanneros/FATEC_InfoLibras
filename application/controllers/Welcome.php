513a4ed46f6362b53f658460d45930155040bc57
allanneros <allan@neros.com.br>
2022-05-07 03:57:09 -0300

    v1.18 Alterações na landing page

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -4,14 +4,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Welcome extends CI_Controller {
 
 	public function index() {
-		$sessao = lerSessaoAtual();
-
-		if ($sessao) {
-			redirect(base_url() . index_page() . '/inicio');
-
-		} else {
-			$this->load->view('welcome_message');
-
-		}
+		$this->load->view('welcome_message');
 	}
 }
